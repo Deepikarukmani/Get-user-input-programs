6a1b278eaa7178940134f508d7686818b61fd701
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-17 20:21:05 +0530

    Add files via upload

---
+++ examples of to get the input from user.py
@@ -0,0 +1,41 @@
+#Get input for variable name and age, and print it:
+name = input()
+age = input()
+print("My Name is:",name)
+print("My Age is:" ,age)
+
+#Get input for variable name and age, address and print it:
+name = input()
+age = input()
+address = input()
+print("My Name is:",name)
+print("My Age is:" ,age)
+print("My address is:" ,address)
+
+
+#Get integer input for variable a,b,c.
+#multiply all 3 variables (a*b*c)
+#add all 3 variables (a+b+c)
+#divide the multiplied values by added values and print it
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = (a*b*c)
+e = (a+b+c)
+f = (d/e)
+print(d)
+print(e)
+print(f)
+
+
+#Get input for variable name, score, department
+#get score for 100:
+#divide 100/10 and print it
+
+name = input()
+score = int(input())
+department = input()
+print("my name is ", name)
+print("my score is ", score/10,"/10")
+print("my department is ", department)
